8a1ba7e4923c83d754e21feca25e94e5ba77f234
asseternity <nakupovasset@gmail.com>
2025-02-02 21:25:35 +0400

    feat: init project, content and sample css

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+export default function App() {
+  const [someState, setSomeState] = useState('');
+  return (
+    <div className="root">
+      <div className="top">
+        <div className="top_left">
+          <h1>Hi, I'm Asset</h1>
+          <p>
+            I’m a self-taught full-stack developer with expertise in front-end
+            (React, JavaScript, Jest, HTML/CSS), back-end (Node.js, Express.js,
+            Python, C#), and database technologies (SQL, PostgreSQL, Prisma
+            ORM). IELTS 8.5.
+          </p>
+          <p>
+            After a successful 7+ year career as a Senior Legal Associate at a
+            top 10 global law firm, I transitioned into tech, applying my
+            analytical, project management, and problem-solving skills to
+            full-stack development.
+          </p>
+          <button>Github</button>
+          <button>Linkedin</button>
+        </div>
+        <div className="top_right">
+          <img />
+        </div>
+      </div>
+      <div className="projects">
+        <h3>Projects</h3>
+        <div className="projects_cards">
+          <div className="projects_card"></div>
+          <div className="projects_card"></div>
+          <div className="projects_card"></div>
+          <div className="projects_card"></div>
+          <div className="projects_card"></div>
+          <div className="projects_card"></div>
+        </div>
+      </div>
+      <div className="bottom">
+        <h3>Get in touch</h3>
+        <p>email</p>
+        <p>location</p>
+        <button>Github</button>
+        <button>Linkedin</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+// [_] project templates
+// [_] layout
+// [_] projects
+// [_] favicon
+// [_] interactivity - some WHOA moving thing
