39f98499551dc0954c93b070c04a5249bc4b7355
asseternity <nakupovasset@gmail.com>
2025-04-14 17:54:52 +0400

    feat: create tagline page, commence work on tech stack card page

--- src/components/tech_stack.jsx
+++ src/components/tech_stack.jsx
@@ -1,4 +1,17 @@
 import html from '/tech stack icons/html5-01-svgrepo-com.svg';
+import css from '/tech stack icons/css3-01-svgrepo-com.svg';
+import js from '/tech stack icons/javascript-16-svgrepo-com.svg';
+import jest from '/tech stack icons/testing-flask-svgrepo-com.svg';
+import react from '/tech stack icons/react-16-svgrepo-com.svg';
+import node from '/tech stack icons/node-js-svgrepo-com.svg';
+import express from '/tech stack icons/express-svgrepo-com.svg';
+import python from '/tech stack icons/python-svgrepo-com.svg';
+import csharp from '/tech stack icons/csharp-svgrepo-com.svg';
+import restapi from '/tech stack icons/api-svgrepo-com.svg';
+import unity from '/tech stack icons/unity-svgrepo-com.svg';
+import sql from '/tech stack icons/database-svgrepo-com.svg';
+import postgresql from '/tech stack icons/postgresql-svgrepo-com.svg';
+import prismaORM from '/tech stack icons/db-file-format-symbol-svgrepo-com.svg';
 import TechStackCard from './tech_stack_card';
 
 export default function TechStack() {
@@ -8,7 +21,10 @@ export default function TechStack() {
       <h4>Frontend:</h4>
       <ul>
         <TechStackCard image={html} text={'HTML'} />
-        <li>CSS</li>
+        <TechStackCard image={css} text={'CSS'} />
+        <TechStackCard image={js} text={'JavaScript'} />
+        <TechStackCard image={jest} text={'Jest'} />
+        <TechStackCard image={react} text={'React'} />
         <li>JavaScript</li>
         <li>Jest</li>
         <li>React</li>
